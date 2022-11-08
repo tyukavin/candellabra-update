11301e124adcbd26bfa55d544c46f0c7b30e8032
Denis Tyukavin <dtyukavin@dmvdesk.com>
2022-11-09 02:15:25 +0300

    card item step 2

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -6,4 +6,26 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
     });
 
+    $('.js-slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.js-slider-nav',
+        adaptiveHeight: true,
+        draggable: false,
+    });
+
+    $('.js-slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: '.product-slider__step--prev',
+        nextArrow: '.product-slider__step--next',
+        asNavFor: '.js-slider-main',
+        focusOnSelect: true,
+        vertical: true,
+        adaptiveHeight: true,
+        centerMode: true,
+    });
+
 });
